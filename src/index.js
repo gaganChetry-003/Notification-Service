e95c867b68be2b944db650acc687845aafa3a4dc
gagan_003 <gaganchetry0702@gmail.com>
2023-07-29 20:49:40 +0530

    emailing servivr working fine

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const mailsender = require('./config/email-config')
 const {ServerConfig}=require('./config');
 const apiRoutes=require('./routes');
 
@@ -8,6 +9,18 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api',apiRoutes);
-app.listen(ServerConfig.PORT,()=>{
-    console.log(`Successfully started the server:${ServerConfig.PORT}`)
+app.listen(ServerConfig.PORT,async()=>{
+    console.log(`Successfully started the server:${ServerConfig.PORT}`);
+    try{
+        const response=await mailsender.sendMail({
+            from:ServerConfig.GMAIL_EMAIL,
+            to:'bishalchetry4212@gmail.com',
+            subject:'Is the service working',
+            text:'YES its working'
+        });
+        console.log(response);
+    }catch(error){
+     console.log(error);
+    }
+   
 })
